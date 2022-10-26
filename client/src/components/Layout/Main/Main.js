bf69c746e8ce39cb018a5e44a96c5d13aa89f7d1
Exist95 <jyeol0210@naver.com>
2022-10-26 12:25:50 +0900

    💩 : 메인 로직 연습(수정 예정)

--- client/src/components/Layout/Main/Main.js
+++ client/src/components/Layout/Main/Main.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { MainBox } from './Main.style';
+import { dummy } from './Dummy';
+import { MainBox, MainMiniBox } from './Main.style';
 
 const Main = () => {
-  return <MainBox>Main</MainBox>;
+  return (
+    <MainBox>
+      <MainMiniBox>
+        {dummy.map((data, i) => {
+          return (
+            <div key={i}>
+              <div>{data.id}</div>
+              <div>{data.title}</div>
+              <div>{data.contents}</div>
+              <div>{data.createAt}</div>
+              <div>{data.answers}</div>
+            </div>
+          );
+        })}
+      </MainMiniBox>
+    </MainBox>
+  );
 };
 
 export default Main;
--- client/src/components/Layout/Main/Main.style.js
+++ client/src/components/Layout/Main/Main.style.js
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 
 export const MainBox = styled.div`
-  width: 1000px;
-  height: 1000px;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MainMiniBox = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 50vw;
+  height: 100px;
+  background-color: red;
 `;
