01ae914a61ab6e3eea5f02ca911ceb7400526541
jisu Ha <jisuha@jisuui-MacBookPro.local>
2022-10-26 15:38:09 +0900

    ✨경로 추가

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Login from './pages/login';
 import Signup from './pages/signup';
+import Tags from './pages/Tags';
+import Users from './pages/Users';
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
         <Route path="/" element={<Layout />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/tags" element={<Tags />} />
+        <Route path="/users" element={<Users />} />
       </Routes>
     </RecoilRoot>
   );
--- client/src/components/Layout/SideBar/LeftSide.js
+++ client/src/components/Layout/SideBar/LeftSide.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { LeftBar, QuestionIcon } from './LeftSide.style';
 import ad from '../../../assets/images/ad.png';
+import { Link } from 'react-router-dom';
 
 const LeftSide = () => {
   return (
     <LeftBar>
       <ul>
         <QuestionIcon />
-        <li>Questions</li>
-        <li>Tags</li>
-        <li>Users</li>
+        <li>
+          <Link to="/">Questions</Link>
+        </li>
+
+        <li>
+          <Link to="/tags">Tags</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
         <div>Stack Overflow for Teams</div>
         <span>Start collaborating and sharing organizational knowledge.</span>
         <img src={ad} alt="" />
-        <button>Create free Team</button>
+        <button className="team">Create free Team</button>
       </ul>
     </LeftBar>
   );
