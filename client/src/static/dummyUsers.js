f060f6f58fb50deb800a59965014e4eead3784fe
heoputer <hjw1753@gmail.com>
2022-10-28 13:57:20 +0900

    💩 : 유저 더미데이터 작성

---
+++ client/src/static/dummyUsers.js
@@ -0,0 +1,111 @@
+const dummyUsers = [
+  {
+    id: 1,
+    name: '허정우',
+    email: '_heojeong@kakao.com',
+    createAt: '2022-10-26',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 2,
+    name: '김정우',
+    email: '_heojeong@kakao.com',
+    createAt: '2022-10-25',
+    img: 'https://i.stack.imgur.com/I4fiW.jpg?s=96&g=1',
+  },
+  {
+    id: 3,
+    name: '이정우',
+    email: '_heojeong@kakao.com',
+    createAt: '2022-10-1',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 4,
+    name: '박정우',
+    email: '_heojeong@kakao.com',
+    createAt: '2022-9-26',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 5,
+    name: '최정우',
+    email: '_heojeong@kakao.com',
+    createAt: '2022-10-26',
+    img: 'https://www.gravatar.com/avatar/0555bd0deb416a320a0069abef08078a?s=96&d=identicon&r=PG&f=1',
+  },
+  {
+    id: 6,
+    name: '민정우',
+    createAt: '2022-10-26',
+    img: 'https://i.stack.imgur.com/hMDvl.jpg?s=96&g=1',
+  },
+  {
+    id: 7,
+    name: 'javascript',
+    createAt: '2022-10-25',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 8,
+    name: 'javascript',
+    createAt: '2022-10-25',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 9,
+    name: 'javascript',
+    createAt: '2022-10-25',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 10,
+    name: 'javascript',
+    createAt: '2022-10-25',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 11,
+    name: 'javascript',
+    createAt: '2022-10-25',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 12,
+    name: 'javascript',
+    createAt: '2022-10-25',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 13,
+    name: 'a',
+    createAt: '2022-10-25',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 14,
+    name: 'b',
+    createAt: '2022-10-25',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 15,
+    name: 'c',
+    createAt: '2022-10-25',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 16,
+    name: 'd',
+    createAt: '2022-10-25',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+  {
+    id: 17,
+    name: 'e',
+    createAt: '2022-10-25',
+    img: 'https://avatars.githubusercontent.com/u/58350169?v=4',
+  },
+];
+
+export default dummyUsers;
