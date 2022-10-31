88052649e43b0eb3d82d9d43ca70f83c4c8c51c0
inmyownway <wjdals05070@gmail.com>
2022-10-31 13:56:55 +0900

    ✨ : 답변좋아요 추가

--- server/src/main/java/com/iphone/server/answer_like/repository/Answer_likeRepository.java
+++ server/src/main/java/com/iphone/server/answer_like/repository/Answer_likeRepository.java
@@ -3,4 +3,5 @@ package com.iphone.server.answer_like.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface Answer_likeRepository extends JpaRepository {
+    //
 }
