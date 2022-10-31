1b89f7029b7dd2384173a259f5d55aa95b3865b6
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-10-31 13:31:29 +0900

    ♻️ : line clean

--- server/src/main/java/com/iphone/server/user/controller/UserController.java
+++ server/src/main/java/com/iphone/server/user/controller/UserController.java
@@ -13,38 +13,39 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
+
     @PostMapping
-    public UserRegisterResponse registerUser(@RequestBody UserRegisterRequest userRegisterRequest){
+    public UserRegisterResponse registerUser(@RequestBody UserRegisterRequest userRegisterRequest) {
         return userService.UserRegister(userRegisterRequest);
     }
 
     @GetMapping("/auth/email/{email}")
-    public UserExistResponse duplicateCheckEmail(@PathVariable(value="email") String email){
+    public UserExistResponse duplicateCheckEmail(@PathVariable(value = "email") String email) {
         return userService.emailDuplicateCheck(email);
     }
 
     @GetMapping("/auth/nick/{nick}")
-    public UserExistResponse duplicateCheckNickName(@PathVariable(value = "nick") String nick){
+    public UserExistResponse duplicateCheckNickName(@PathVariable(value = "nick") String nick) {
         return userService.nickDuplicateCheck(nick);
     }
 
     @PostMapping("/authentication")
-    public LoginResponse login(@RequestBody LoginRequest loginRequest){
+    public LoginResponse login(@RequestBody LoginRequest loginRequest) {
         return userService.loginResponse(loginRequest);
     }
 
     @DeleteMapping("/authentication")
-    public ResponseEntity<?> logout(){
+    public ResponseEntity<?> logout() {
         return userService.logoutResponse();
     }
 
     @GetMapping
-    public Page<UserInfoResponse> getAllUsers(Pageable pageable){
+    public Page<UserInfoResponse> getAllUsers(Pageable pageable) {
         return userService.getUsers(pageable);
     }
 
     @GetMapping("/nickname/{nickname}")
-    public Page<UserInfoResponse> getAllUsersByNickname(Pageable pageable,@PathVariable String nickname){
-        return userService.getUsersByNickname(pageable,nickname);
+    public Page<UserInfoResponse> getAllUsersByNickname(Pageable pageable, @PathVariable String nickname) {
+        return userService.getUsersByNickname(pageable, nickname);
     }
 }
