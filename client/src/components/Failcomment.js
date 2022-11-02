17a579160d36a960a388d640834e14f0296885e8
heoputer <hjw1753@gmail.com>
2022-11-02 16:54:04 +0900

    🎨 : 로그인 실패 메시지 컴포넌트화

---
+++ client/src/components/Failcomment.js
@@ -0,0 +1,17 @@
+const Failcomment = ({ comment }) => {
+  return (
+    <div
+      style={{
+        color: 'rgb(208, 57, 62)',
+        fontSize: '13px',
+        fontWeight: 'bold',
+        marginTop: '5px',
+        marginBottom: '15px',
+      }}
+    >
+      {comment}
+    </div>
+  );
+};
+
+export default Failcomment;
