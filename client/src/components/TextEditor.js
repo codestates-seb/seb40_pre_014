ed28fbdc017f5801070fc9a020b1cbde0f360f70
heoputer <hjw1753@gmail.com>
2022-11-03 14:43:14 +0900

    ✨ : 서버 통신을 위한 질문 본문 데이터화

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -1,14 +1,22 @@
+import React, { useState } from 'react';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
-const TextEditor = ({ height }) => {
+export default function TextEditor({ height }) {
+  const [text, setText] = useState('');
+  const editorRef = React.createRef();
+  const onChange = () => {
+    const data = editorRef.current.getInstance().getHTML(); // getHTML or getMarkdown
+    setText(data);
+    console.log(text);
+  };
   return (
     <>
       <Editor
         placeholder="내용을 입력해주세요."
-        previewStyle="vertical" // 미리보기 스타일 지정
-        height={height} // 에디터 창 높이
-        initialEditType="wysiwyg" // 초기 입력모드 설정(디폴트 markdown)
+        previewStyle="vertical" // vertical로 설정시 미리보기 화면 분할가능
+        height={height}
+        initialEditType="markdown" // markdown or wysiwyg
         toolbarItems={[
           // 툴바 옵션 설정
           ['heading', 'bold', 'italic', 'strike'],
@@ -17,9 +25,9 @@ const TextEditor = ({ height }) => {
           ['table', 'link'],
           ['code', 'codeblock'],
         ]}
+        ref={editorRef}
+        onChange={onChange}
       />
     </>
   );
-};
-
-export default TextEditor;
+}
